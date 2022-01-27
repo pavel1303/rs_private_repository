c7eb58f4e0af3e1611fd46ca6b1a545b9a382af1
Pavel Lebedev <lebedpavel.dev@gmail.com>
2022-01-27 15:18:04 +0300

    feat: add switch language

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -1,13 +1,18 @@
-console.log("Итог самопроверки: 110/110 \n -Верстка проходит тестирование на валидность +10 \n -Верстка семантическая, присутствуют все необходимые элементы +20 \n -Верстка соответствует макету в Pixel Perfect +48 \n -Все требования к CSS соблюденны +12 \n -Все требования интерактивности соблюденны +20");
+import i18Obj from "./translite.js";
+
 
 const menu = document.querySelector('.burger');
 const nav = document.querySelector('.navigation');
 const menuItem = document.querySelectorAll('.navigation-item');
 const imgs = document.querySelectorAll('.portfolio__gallary-item');
 const portfolioBtns = document.querySelectorAll('.portfolio__btns-item');
+const seasons = ['winter', 'spring', 'summer', 'autumn'];
+const lang = document.querySelector('.language').querySelectorAll('a');
 
 
 changeImagesAfterClick(imgs, portfolioBtns);
+
+preloaderImages(seasons);
 
 menu.addEventListener('click', () => {
    nav.classList.toggle('active');
@@ -21,12 +26,18 @@ menuItem.forEach(el => {
    })
 })
 
+lang.forEach(el => {
+   el.addEventListener('click', () => {
+      lang.forEach(el => el.classList.remove('checked'));
+      el.classList.add('checked');
+      changeLanguage(el.textContent);
+   })
+})
+
 function changeImagesAfterClick(imgs, btns) {
    btns.forEach(el => {
       el.addEventListener('click', () => {
-         btns.forEach(el => {
-            el.classList.remove('chosen');
-         })
+         btns.forEach(el => el.classList.remove('chosen'));
          el.classList.add('chosen');
          let value = el.dataset.season;
          changeImages(imgs, value);
@@ -39,3 +50,22 @@ function changeImages(imgs, value) {
       el.src = `assets/img/${value}/${index + 1}.jpg`
    })
 }
+
+function preloaderImages(arr) {
+   seasons.forEach(el => {
+      for (let i = 1; i <= 6; i++) {
+         const img = new Image();
+         img.src = `assets/img/${el}/${i}.jpg`;
+      }
+   })
+}
+
+function changeLanguage(lang) {
+   const words = document.body.querySelectorAll('[data-i18n]');
+   words.forEach(el => {
+      el.textContent = i18Obj[lang][el.dataset.i18n];
+   })
+}
+
+
+
