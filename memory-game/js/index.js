890de9a9e3d5b3baadf6310c12419d18530be376
Pavel Lebedev <lebedpavel.dev@gmail.com>
2022-02-16 21:46:57 +0300

    feat: end create main functionality

--- memory-game/js/index.js
+++ memory-game/js/index.js
@@ -1,2 +1,63 @@
 let cards = document.querySelectorAll('.mem-card');
+let game = document.querySelector('.game');
+let hasFlipedCard = false;
+let firstCard, secondCard;
+let locCard = false;
 
+function flipCard(el) {
+   if (locCard) return;
+   if (firstCard === el) return;
+   el.classList.add('flip');
+   if (!hasFlipedCard) {
+      hasFlipedCard = true;
+      firstCard = el;
+      return;
+   } else {
+      secondCard = el;
+      hasFlipedCard = false;
+      if (firstCard.dataset.snoop === secondCard.dataset.snoop) {
+         disableCard();
+      } else {
+         unflipCards();
+      }
+   }
+}
+
+function disableCard() {
+   locCard = true;
+   firstCard.removeEventListener('mousedown', flipCard);
+   firstCard.removeEventListener('mousedown', flipCard);
+   setTimeout(() => {
+      newStep();
+   }, 1500)
+}
+
+function unflipCards() {
+   locCard = true;
+   setTimeout(() => {
+      firstCard.classList.remove('flip');
+      secondCard.classList.remove('flip');
+      newStep();
+   }, 1500)
+}
+function shake() {
+   cards.forEach(card => {
+      card.style.order = Math.floor(Math.random() * 12);
+   });
+}
+
+function newStep() {
+   hasFlipedCard = false;
+   locCard = false;
+
+   firstCard = null;
+   secondCard = null;
+}
+
+game.addEventListener('mousedown', (event) => {
+   let card = event.target.closest('.mem-card');
+   if (!card) return;
+   flipCard(card);
+})
+
+shake();
